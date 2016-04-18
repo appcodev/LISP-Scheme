ce2836ddcf095fb6d4493f181c48f0a95145187d
Appcodev <appcodev@gmail.com>
2016-04-18 18:17:02 +0700

    4 number, complete
    next step, generate state space for 2 and 1 number(s)

--- game-24-solver/game24-solver-v1.scm
+++ game-24-solver/game24-solver-v1.scm
@@ -24,19 +24,147 @@
 ;; 2. if length of remain number is 2 then
 ;;       2.1 operate the previous result with group of rest number
 
+;; state : ((5 1 2 3)()())
+;; (car state) : (5 1 2 3)
 (define (move-pair state)
   (let ((num-list (car state)))
     ;; check length
     (if (and (> (length num-lst) 0)
              (<= (length num-lst) 4))
-        ;; get-pair
+        ;; get-all-pairs
         #t
+        #f
       )))
 
+;; generate step by step
+;; ex. state ((5 1 2 3)()())
+;; 1. get-pair index 0
+;; '() --> skip
+;; 2. get-pair index 1,2,3
+;; get-pair index (1)
+;; ((5 1)(5 2)(5 3))
+;; get-pair index (2)
+;; ((1 2)(1 3))
+;; get-pair index (3)
+;; ((2 3))
+;; all pair = ((5 1)(5 2)(5 3)(1 2)(1 3)(2 3))
+
+;; 3. p = (car get-pair)
+;; operate all operators (+ - * /)
+;; p(0) = (5 1)
+;; calculate (5+1) => ((2 3)(6)(5+1))
+;; calculate (5-1) => ((2 3)(4)(5-1))
+;; calculate (5*1) => ((2 3)(5)(5*1))
+;; calculate (5/1) => ((2 3)(5)(5/1))
+;; p(1) = (5 2) ... continuous operate all operators
+;; p(2) do same p(0) p(1)
+
+;; 4. result is all possible pairs of state
+
+;; ex. state ((5 1 2 3)()())
+;; lst-pair ((5 1)(5 2)(5 3)(1 2)(1 3)(2 3))
+;; (generate-all '((5 1 3 2)()()))
+;; (generate-all '((5 4 4 2)()()))
+
+(define OPs '(+ - x ÷))
+(define (generate-all state)
+  (define (generate lst-pair)
+    (cond ((null? lst-pair) '())
+          (else 
+           (append (cal-all state (car lst-pair) OPs)
+                   (generate (cdr lst-pair))))))
+  ;; body
+  (generate (get-all-pairs state)))
+
+;; all operators
+(define (cal-all state pair lst-op)
+  (cond ((null? lst-op) '())
+        (else
+         (append (cal-op state pair (car lst-op))
+                 (cal-all state pair (cdr lst-op))))))
+
+;; one operator
+(define (cal-op state pair op)
+  (let ((rs (apply (symbol->operator op) pair)))
+    (if (integer? rs)
+        (list (list (remove-all (car state) 
+                                (remove-all pair (cadr state)))
+                    (list (apply (symbol->operator op) pair))
+                    (list (list (car pair) op (cadr pair)))))
+        '())))
+
+;; pair index 0 to n-1
+;; where n is length of (car state)
+;; ex.
+;; >  (get-all-pairs '((5 3 1 2)()()))
+;; ((5 3) (5 1) (5 2) (3 1) (3 2) (2 1))
+;; >  (get-all-pairs '((5 3 3 2)()()))
+;; ((5 3) (5 2) (3 3) (3 2))
+;; >  (get-all-pairs '((4 4 4 3)()()))
+;; ((4 4) (4 3))
+;; >  (get-all-pairs '((3 3 3 3)()()))
+;; ((3 3))
+
+(define (get-all-pairs state)
+  (define (all-pairs index)
+    (cond ((>= index (length (car state))) '())
+          (else
+           (append (get-pair state index)
+                   (all-pairs (+ index 1))))))
+  (remove-dup (all-pairs 0)))
+
+;; state : ((5 1 2 3)()())
+;; index : 0
+;; output : ()
+;; index : 1
+;; output : ((5 1)(5 2)(5 3))
+;; index : 2
+;; output : ((1 2)(1 3))
+;; index : 3 (n-1)
+;; output : ((2 3))
+
+;; state : ((2 3)(6)(5+1))
+;; index : 0
+;; output : ((6 2)(6 3))
+;; index : 1 (n-1)
+;; output : ((2 3))
+
 (define (get-pair state index)
-  #f
-  
-  )
+  (let ((x (get-x state index)))
+    ;; inner function
+    (define (make-pair state x next-index)
+      (let ((next (get-x state next-index)))
+        (cond ((null? next) '())
+              (else
+               (cons (sort > (list x next))
+                     (make-pair state x (+ next-index 1)))))))
+    ;; body
+    (cond ((null? x) '())
+          (else
+           (make-pair state x (+ index 1))))))
+
+;; state : ((5 1 2 3)()())
+;; index 0 : ()
+;; index 1 : 5
+;; index 2 : 1
+;; index 3 : 2
+;; index 4 : 3
+
+;; state : ((2 3)(6)(5+1))
+;; index 0 : 6
+;; index 1 : 2
+;; index 2 : 3
+
+(define (get-x state index)
+  (if (and (>= index 0)
+           (<= index (length (car state))))
+      (cond ((= index 0) 
+             (if (null? (cadr state))
+                 '() 
+                 (caadr state)))
+            (else
+             (list-ref (car state) (- index 1))))
+      '()))
 
 
 
--- game-24-solver/my-func.ss
+++ game-24-solver/my-func.ss
@@ -13,8 +13,16 @@
            (cons (car lst)
                  (replace (cdr lst) (- index 1) rep-with)))))
   
-  ;; remove
-  (define (remove! lst lst-index)
+  ;; remove-all
+  (define (remove-all lst lst-rmv)
+    (cond ((null? lst-rmv) lst)
+          (else
+           (remove-all (remove (car lst-rmv) lst)
+                       (cdr lst-rmv)))))
+  
+  
+  ;; remove-all-by-index
+  (define (remove-all-indx lst lst-index)
     (remove-lst lst
                 (sort < lst-index)))
   
@@ -33,7 +41,39 @@
            (cons (car lst)
                  (remove-x (cdr lst) (- index 1))))))
   
+  ;; remove duplicate
+  ;; ((5 3) (5 3) (5 2) (3 3) (3 2) (3 2))
+  ;; > ((5 3) (5 2) (3 3) (3 2))
+  (define (remove-dup lst)
+    (cond ((null? lst) '())
+          ((member (car lst) (cdr lst))
+           (remove-dup (cdr lst)))
+          (else
+           (cons (car lst)
+                 (remove-dup (cdr lst))))
+          ))
+  
+  ;; symbol->operator
+  (define (symbol->operator op)
+    (cond ((eq? op '+) +)
+          ((eq? op '-) -)
+          ((eq? op 'x) *)
+          ((eq? op '÷) /)
+          (else #f)))
+  
+  ;; operator->symbol
+  (define (operator->symbol op)
+    (cond ((equal? op +) '+)
+          ((equal? op -) '-)
+          ((equal? op *) 'x)
+          ((equal? op /) '÷)
+          (else #f)))
   
   ;; export
   (provide replace)
-  (provide remove!))
+  (provide remove-all)
+  (provide remove-all-indx)
+  (provide remove-dup)
+  (provide symbol->operator)
+  (provide operator->symbol)
+  (provide sort))
